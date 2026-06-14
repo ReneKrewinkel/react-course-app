1c37e03b4e7fe7df289e4800d64703b9ef1ec90b
René Krewinkel <rene@studiokrijst.com>
2026-06-14 15:39:03 +0200

    update

--- src/components/atoms/Label/Label.interface.tsx
+++
@@ -1,9 +0,0 @@
-import type { ComponentPropsWithoutRef, ReactNode } from 'react'
-
-export interface LabelInterface extends Omit<ComponentPropsWithoutRef<'label'>, 'children'> {
-    helperText?: string,
-    required?: boolean,
-    size?: 's' | 'm' | 'l',
-    testID?: string,
-    text: ReactNode,
-}
